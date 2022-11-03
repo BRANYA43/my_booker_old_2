609c3b09033af7763b3faadcafbfb46890dceb92
Timchenko <tbd2504@gmail.com>
2022-11-03 17:06:01 +0300

    Create class MainFrame

---
+++ views/main_frame.py
@@ -0,0 +1,24 @@
+import wx
+
+import config.config as cfg
+
+
+class MainFrame(wx.Frame):
+    def __init__(self):
+        super().__init__(parent=None, title=cfg.TITLE, size=cfg.SIZE_FRAME)
+        notebook = wx.Notebook(self)
+        tab_test_0 = wx.Panel(notebook)
+        tab_test_1 = wx.Panel(notebook)
+        notebook.InsertPage(0, tab_test_0, 'Home')
+        notebook.InsertPage(1, tab_test_1, 'Setting')
+
+    def show_frame(self):
+        self.Center()
+        self.Show()
+
+
+if __name__ == '__main__':
+    app = wx.App()
+    main_frame = MainFrame()
+    main_frame.show_frame()
+    app.MainLoop()
