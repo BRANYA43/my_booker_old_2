338796e9a4e57d0d4791476d341e95eebfa47c01
Timchenko <tbd2504@gmail.com>
2022-11-04 15:31:59 +0300

    Add localization EN

---
+++ lang/en.py
@@ -0,0 +1,50 @@
+NAMES = {
+    # A
+    'add': 'Add',
+    # B
+    # C
+    'calculate': 'Calculate',
+    'cost': 'Cost',
+    'count': 'Count',
+    # D
+    'delete': 'Delete',
+    'detail': 'Detail',
+    # E
+    'edit': 'Edit',
+    # F
+    # G
+    # H
+    # I
+    'info': 'Info',
+    'id': 'ID',
+    'id_d': 'ID detail',
+    'id_p': 'ID product',
+    'id_l': 'ID laborer',
+    'id_o': 'ID operation',
+    # J
+    # L
+    'laborer': 'Laborer',
+    # K
+    # M
+    # N
+    'no': 'No',
+    'name': 'Name',
+    # O
+    'ok': 'OK',
+    'operation': 'Operation',
+    # P
+    'product': 'Product',
+    # Q
+    # R
+    # S
+    'save': 'Save',
+    'salary': 'Salary',
+    # T
+    # U
+    # V
+    # W
+    # X
+    # Y
+    'yes': 'Yes',
+    # Z
+}
