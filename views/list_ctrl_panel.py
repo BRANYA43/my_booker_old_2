1b3fde22e96a166352366dbe30806ec516e0fc5f
Timchenko <tbd2504@gmail.com>
2022-11-05 13:26:47 +0300

    Refactor class ListCtrlPanel
    class ListCtrlPanel inherits to class Constructor

--- views/list_ctrl_panel.py
+++ views/list_ctrl_panel.py
@@ -2,47 +2,27 @@ import wx
 
 import config.config as cfg
 import lang.en as lang
+from objects.constructor import Constructor
 
 
-class ListCtrlPanel(wx.Panel):
+class ListCtrlPanel(Constructor):
     def __init__(self, parent, size_list=cfg.SIZE_LIST):
-        super().__init__(parent=parent)
-        self.btns: dict[str: wx.Button] = {}
+        super().__init__(window=1, parent=parent)
         self.columns: dict[str: int] = {}
         self.list_ctrl = wx.ListCtrl(self, size=size_list, style=wx.LC_REPORT)
-        self.build_panel()
-
-    def add_button(self, *labels):
-        for label in labels:
-            self.btns.setdefault(label, wx.Button(self, label=lang.NAMES[label]))
-
-    def enable_button(self, *labels):
-        for label in labels:
-            self.btns[label].Enable(True)
-
-    def disable_button(self, *labels):
-        for label in labels:
-            self.btns[label].Enable(False)
-
-    def bind_button(self, func, label):
-        button = self.btns[label]
-        self.Bind(wx.EVT_BUTTON, func, button)
+        self.build_view()
 
     def add_column(self, *headers):
         for col, header in enumerate(headers):
             self.list_ctrl.InsertColumn(col, lang.NAMES[header])
             self.columns.setdefault(header, col)
 
-    def hide_column(self, *headers):
-        self.width_column(width=0, *headers)
-
     def width_column(self, width: int, *headers):
         for header in headers:
             self.list_ctrl.SetColumnWidth(col=self.columns[header], width=width)
 
-    def get_row(self, row: int) -> tuple:
-        cols = self.list_ctrl.GetColumnCount()
-        return tuple(self.list_ctrl.GetItem(row, col).GetText() for col in range(cols))
+    def hide_column(self, *headers):
+        self.width_column(width=0, *headers)
 
     def add_row(self, *items):
         assert len(items) == self.list_ctrl.GetColumnCount()
@@ -52,5 +32,6 @@ class ListCtrlPanel(wx.Panel):
     def del_row(self, row: int):
         self.list_ctrl.DeleteItem(row)
 
-    def build_panel(self):
-        raise NotImplemented
+    def get_row(self, row: int) -> tuple:
+        cols = self.list_ctrl.GetColumnCount()
+        return tuple(self.list_ctrl.GetItem(row, col).GetText() for col in range(cols))
