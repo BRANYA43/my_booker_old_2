8694f96a245abf6a10c3f96448ace8c5fd187f3e
Timchenko <tbd2504@gmail.com>
2022-11-07 23:34:03 +0200

    Added arguments in func __init__()

--- constructors/panel_constructor.py
+++ constructors/panel_constructor.py
@@ -5,8 +5,8 @@ from constructors.constructor import Constructor
 
 
 class PanelConstructor(wx.Panel, Constructor):
-    def __init__(self, parent):
-        wx.Panel.__init__(self, parent, size=wx.DefaultSize, style=wx.TAB_TRAVERSAL)
+    def __init__(self, parent, size=wx.DefaultSize, style=wx.TAB_TRAVERSAL):
+        wx.Panel.__init__(self, parent, size=size, style=style)
         Constructor.__init__(self)
         font = wx.SystemSettings.GetFont(wx.SYS_DEFAULT_GUI_FONT)
         font.SetPointSize(cfg.SIZE_FONT)
