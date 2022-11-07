63df4779b672100e07eb6fca5ff9af1e2a400b99
Timchenko <tbd2504@gmail.com>
2022-11-07 22:41:40 +0200

    small refactor

--- constructors/list_constructor.py
+++ constructors/list_constructor.py
@@ -2,16 +2,15 @@ import wx
 
 import config.config as cfg
 import lang.en as lang
-from constructors.constructor import Constructor
+from constructors.panel_constructor import PanelConstructor
 from funcs_support import reiterate_func
 
 
-class ListConstructor(wx.Panel, Constructor):
+class ListConstructor(PanelConstructor):
     def __init__(self, parent, size_list=cfg.SIZE_LIST):
-        wx.Panel.__init__(parent)
+        super().__init__(parent=parent)
         self.columns: dict[str: int] = {}
         self.list_ctrl = wx.ListCtrl(self, size=size_list, style=wx.LC_REPORT)
-        self.build_view()
 
     @reiterate_func
     def add_column(self, header):
@@ -41,3 +40,7 @@ class ListConstructor(wx.Panel, Constructor):
     def get_row(self, row: int) -> tuple:
         cols = self.list_ctrl.GetColumnCount()
         return tuple(self.list_ctrl.GetItem(row, col).GetText() for col in range(cols))
+
+
+if __name__ == '__main__':
+    ...
