0528c0e752b90d0d3012014b92353e8b39d1d941
Timchenko <tbd2504@gmail.com>
2022-11-07 18:31:51 +0200

    Renamed file and created directory constructors

---
+++ constructors/constructor.py
@@ -0,0 +1,50 @@
+import wx
+
+import config.config as cfg
+import lang.en as lang
+from funcs_support import reiterate_func
+
+
+class Constructor:
+    def __init__(self):
+        self.btns = {}
+        self.comboboxes = {}
+        self.static_labels = {}
+        self.entry_fields = {}
+
+    @reiterate_func
+    def add_btn(self, name: str):
+        key = name
+        name = lang.NAMES[name]
+        self.btns.setdefault(key, wx.Button(self, label=name))
+
+    def bind_btn(self,  name: str, func):
+        button = self.btns[name]
+        self.Bind(wx.EVT_BUTTON, func, button)
+
+    @reiterate_func
+    def enable_btn(self, name: str):
+        self.btns[name].Enable(True)
+
+    @reiterate_func
+    def disable_btn(self, name: str):
+        self.btns[name].Enable(False)
+
+    def add_combobox(self, name: str, choices: list[str]):
+        self.comboboxes.setdefault(name, wx.ComboBox(self, value=choices[0], choices=choices, style=wx.CB_READONLY))
+
+    @reiterate_func
+    def add_static_label(self, name: str):
+        key = name
+        name = lang.NAMES[name] + ':'
+        self.static_labels.setdefault(key, wx.StaticText(self, label=name))
+
+    @reiterate_func
+    def add_entry_field(self, name: str):
+        self.entry_fields.setdefault(name, wx.TextCtrl(self))
+
+    def add_widgets(self):
+        raise NotImplemented
+
+    def build_view(self):
+        raise NotImplemented
--- views/form_dialog.py
+++
@@ -1,8 +0,0 @@
-import config.config as cfg
-from views.constructor import Constructor
-
-
-class FormDialog(Constructor):
-    def __init__(self, parent, size=cfg.SIZE_DIALOG):
-        super().__init__(window=2, parent=parent, size=size)
-        self.build_view()
