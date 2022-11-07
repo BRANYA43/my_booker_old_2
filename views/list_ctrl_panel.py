d63bc10b79f60b96b488761b7eb7ddeff88f00f2
Timchenko <tbd2504@gmail.com>
2022-11-07 13:21:24 +0200

    Added @reitarate_fucn some funcs.
    - add_column()
    - width_column()
    - hide_column()
    - add_row()
    - del_row()
    - get_row()

--- views/list_ctrl_panel.py
+++ views/list_ctrl_panel.py
@@ -2,7 +2,7 @@ import wx
 
 import config.config as cfg
 import lang.en as lang
-from objects.constructor import Constructor
+from views.constructor import Constructor, reiterate_func
 
 
 class ListCtrlPanel(Constructor):
@@ -12,26 +12,31 @@ class ListCtrlPanel(Constructor):
         self.list_ctrl = wx.ListCtrl(self, size=size_list, style=wx.LC_REPORT)
         self.build_view()
 
-    def add_column(self, *headers):
-        for col, header in enumerate(headers):
-            self.list_ctrl.InsertColumn(col, lang.NAMES[header])
-            self.columns.setdefault(header, col)
+    @reiterate_func
+    def add_column(self, header):
+        col = self.list_ctrl.GetColumnCount()
+        self.list_ctrl.InsertColumn(col, lang.NAMES[header])
+        self.columns.setdefault(header, col)
 
-    def width_column(self, width: int, *headers):
-        for header in headers:
-            self.list_ctrl.SetColumnWidth(col=self.columns[header], width=width)
+    @reiterate_func
+    def width_column(self, header: str, width: int):
+        self.list_ctrl.SetColumnWidth(col=self.columns[header], width=width)
 
-    def hide_column(self, *headers):
-        self.width_column(width=0, *headers)
+    @reiterate_func
+    def hide_column(self, header: str):
+        self.width_column(header, width=0)
 
+    @reiterate_func
     def add_row(self, *items):
         assert len(items) == self.list_ctrl.GetColumnCount()
         row = self.list_ctrl.GetItemCount()
-        self.list_ctrl.SetItemData(self.list_ctrl.Append(items), row)
+        self.list_ctrl.SetItemData(self.list_ctrl.Append(*items), row)
 
+    @reiterate_func
     def del_row(self, row: int):
         self.list_ctrl.DeleteItem(row)
 
+    @reiterate_func
     def get_row(self, row: int) -> tuple:
         cols = self.list_ctrl.GetColumnCount()
         return tuple(self.list_ctrl.GetItem(row, col).GetText() for col in range(cols))
