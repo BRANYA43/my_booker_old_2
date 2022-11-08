6eed72914618009f63944ce2a489701635f99b57
Timchenko <tbd2504@gmail.com>
2022-11-08 13:02:39 +0200

    Refactor add_widgets() and build_view()

--- views/list_laborer.py
+++ views/list_laborer.py
@@ -11,20 +11,22 @@ class ListLaborer(ListConstructor):
         self.build_view()
 
     def add_widgets(self):
+        self.set_size_list(240, 330)
         self.add_column('laborer', 'id')
-        self.set_width_column('laborer', width=200)
+        self.set_width_column('laborer', width=240)
         self.hide_column('id')
         self.add_btn('info', 'add', 'edit', 'delete')
 
     def build_view(self):
+        border = 10
         main_sizer = wx.BoxSizer(wx.VERTICAL)
-        main_sizer.Add(self.list_ctrl, flag=wx.ALIGN_CENTRE, border=10)
+        main_sizer.Add(self.list_ctrl, flag=wx.ALIGN_CENTRE | wx.TOP, border=border)
         btn_sizer = wx.BoxSizer(wx.VERTICAL)
-        btn_sizer.Add(self.btns['info'])
-        btn_sizer.Add(self.btns['add'])
-        btn_sizer.Add(self.btns['edit'])
+        btn_sizer.Add(self.btns['info'], flag=wx.TOP | wx.BOTTOM, border=border)
+        btn_sizer.Add(self.btns['add'], flag=wx.BOTTOM, border=border)
+        btn_sizer.Add(self.btns['edit'], flag=wx.BOTTOM, border=border)
         btn_sizer.Add(self.btns['delete'])
-        main_sizer.Add(btn_sizer, flag=wx.ALIGN_CENTER, border=10)
+        main_sizer.Add(btn_sizer, flag=wx.ALIGN_CENTER, border=border)
         self.SetSizer(main_sizer)
 
 
