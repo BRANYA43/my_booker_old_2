c912145a17feb3404e4b65a3316d3a8cf7340e08
Timchenko <tbd2504@gmail.com>
2022-11-17 16:28:58 +0200

    Refactored: BList classes
    added methods:
    - set_width_group_columns
    
    refactored methods:
    - set_size_list
    - add_column
    - hide_column
    - get_row
    - set_row
    
    moved methods:
    - del_row
    
    decoreted methods:
    - add_column
    - set_width_column
    - hide_column
    - get_row
    - del_row

--- builders/list.py
+++ builders/list.py
@@ -1,9 +1,9 @@
+from abc import abstractmethod
+
 import wx
 
-import config.config as cfg
-import lang.en as lang
 from builders.panel import BPanel
-from funcs_support import reiterate_func
+from support_tools.funcs import reiterate_func_with_one_arg, reiterate_func_with_two_args, reiterate_get_func_with_one_arg
 
 
 class BList(BPanel):
@@ -12,40 +12,61 @@ class BList(BPanel):
         self.columns = {}
         self.list_ctrl = wx.ListCtrl(self, style=wx.LC_REPORT)
 
+    @abstractmethod
+    def create_widgets(self):
+        pass
+
+    @abstractmethod
+    def build_view(self):
+        pass
+
     def set_size_list(self, width: int, height: int):
+        self.list_ctrl.SetSize((width, height))
         self.list_ctrl.SetMinSize((width, height))
 
-    @reiterate_func
+    @reiterate_func_with_one_arg
     def add_column(self, header: str):
         col = self.list_ctrl.GetColumnCount()
-        self.list_ctrl.InsertColumn(col, lang.NAMES[header])
+        self.list_ctrl.InsertColumn(col, header)
         self.columns.setdefault(header, col)
 
-    @reiterate_func
+    @reiterate_func_with_two_args
     def set_width_column(self, header: str, width: int):
         self.list_ctrl.SetColumnWidth(col=self.columns[header], width=width)
 
-    @reiterate_func
+    def set_width_group_columns(self, *headers, width: int):
+        for header in headers:
+            self.set_width_column(header, width)
+
+    @reiterate_func_with_one_arg
     def hide_column(self, header: str):
-        self.set_width_column(header, width=0)
+        width = 0  # Decorator reiterate_func_with_two_args get error because width argument, which transferred how key value in the function of
+        # the set_width_column
+        self.set_width_column(header, width)
 
     def add_row(self, *items):
         assert len(items) == self.list_ctrl.GetColumnCount()
         row = self.list_ctrl.GetItemCount()
         self.list_ctrl.SetItemData(self.list_ctrl.Append(items), row)
 
-    @reiterate_func
+    @reiterate_get_func_with_one_arg
+    def get_row(self, row: int) -> tuple | tuple[tuple] | None | tuple[None]:
+        cols = self.list_ctrl.GetColumnCount()
+        ret = tuple(self.list_ctrl.GetItem(row, col).GetText() for col in range(cols))
+        if len(ret) == 1 and ret[0] == '':
+            ret = None
+        return ret
+
+    def set_row(self, row: int, *items: str, skip_col: tuple = ()):
+        cols = [num for num in self.columns.values() if num not in skip_col]
+        assert len(cols) == len(items)
+        for col, item in zip(cols, items):
+            self.list_ctrl.SetItem(row, col, item[0] if type(item) is list else item)
+
+    @reiterate_func_with_one_arg
     def del_row(self, row: int):
         self.list_ctrl.DeleteItem(row)
 
-    @reiterate_func
-    def get_row(self, row: int) -> tuple:
-        cols = self.list_ctrl.GetColumnCount()
-        return tuple(self.list_ctrl.GetItem(row, col).GetText() for col in range(cols))
-
-    def set_row(self, row: int, *items, skip_col: tuple | None = None):
-        for col, item in enumerate(items):
-            self.list_ctrl.SetItem(row, col, *item if item is list else item)
 
 
 if __name__ == '__main__':
