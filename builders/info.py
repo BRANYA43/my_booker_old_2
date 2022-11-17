f87b7c7cbbd79301f0a248ceb61f5f359d7b535d
Timchenko <tbd2504@gmail.com>
2022-11-17 17:27:26 +0200

    Created: InfoBuilder class

---
+++ builders/info.py
@@ -0,0 +1,34 @@
+from abc import abstractmethod
+
+import wx
+
+from builders.dialog import DialogBuilder
+from support_tools.funcs import reiterate_func_with_one_arg
+
+
+class InfoBuilder(DialogBuilder):
+    def __init__(self, parent):
+        super(InfoBuilder, self).__init__(parent)
+        self.labels = []
+
+    @abstractmethod
+    def create_widgets(self):
+        pass
+
+    @abstractmethod
+    def build_view(self):
+        pass
+
+    @reiterate_func_with_one_arg
+    def create_static_text(self, name: str, *, add=False, auto_name_label=False, colon=False, label='Not named', pos=wx.DefaultPosition, width=-1, height=-1, style=0):
+        super().create_static_text(name, auto_name_label=auto_name_label, colon=colon, label=label, pos=pos, width=width, height=height, style=style)
+        if add:
+            self.labels.append(name)
+
+    def set_labels(self, *values: str):
+        for label, value in zip(self.labels, values):
+            self.get_static_text(label).SetLabel(value)
+
+
+if __name__ == '__main__':
+    ...
