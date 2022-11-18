66ac8c2daf71f466b1ce4c4b8561b6e4046ce0ae
Timchenko <tbd2504@gmail.com>
2022-11-18 13:07:49 +0200

    Refactor: in the class TestFrame of the __init__

--- views/test_frame.py
+++ views/test_frame.py
@@ -2,17 +2,23 @@ import wx
 
 
 class TestFrame(wx.Frame):
-    def __init__(self, window, *attr):
+    def __init__(self, panel=None, dialog=None, *attrs):
         super().__init__(parent=None, title='TEST-FRAME', size=(800, 600))
-        self.window = window(self, *attr)
+        if panel is not None:
+            main_sizer = wx.BoxSizer(wx.VERTICAL)
+            panel = panel(self, *attrs)
+            main_sizer.Add(panel, proportion=1, flag=wx.EXPAND | wx.ALL, border=5)
+            self.SetSizer(main_sizer)
+        elif dialog is not None:
+            self.dialog = dialog(self, *attrs)
 
     def show_frame(self):
         self.Center()
         self.Show()
 
     def show_dialog(self):
-        self.window.Center()
-        self.window.ShowModal()
+        self.dialog.Center()
+        self.dialog.ShowModal()
 
 
 if __name__ == '__main__':
