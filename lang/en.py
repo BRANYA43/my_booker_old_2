ad1d383348eee7917389fb5a08b9f00f69879327
Timchenko <tbd2504@gmail.com>
2022-11-18 09:59:42 +0200

    Refactor: reforme data

--- lang/en.py
+++ lang/en.py
@@ -1,59 +1,51 @@
-NAMES = {
-    # A
-    'add': 'Add',
-    # B
-    # C
-    'calculate': 'Calculate',
-    'cost': 'Cost',
-    'count': 'Count',
-    # D
-    'delete': 'Delete',
-    'detail': 'Detail',
-    # E
-    'edit': 'Edit',
-    # F
-    'full_name': 'Full name',
-    # G
-    # H
-    'hourly_work': 'Hourly work',
-    # I
-    'info': 'Info',
-    'id': 'ID',
-    'id_d': 'ID detail',
-    'id_p': 'ID product',
-    'id_l': 'ID laborer',
-    'id_o': 'ID operation',
-    # J
-    # L
-    'laborer': 'Laborer',
-    'laborers': 'Laborers',
-    # K
-    # M
-    # N
-    'no': 'No',
-    'name': 'Name',
-    # O
-    'ok': 'OK',
-    'operation': 'Operation',
-    # P
-    'product': 'Product',
-    'piecework': 'Piecework',
-    'payment': 'Payment',
-    'pattern_name': 'James Johnson',
-    # Q
-    # R
-    'rate': 'Rate',
-    # S
-    'save': 'Save',
-    'salary': 'Salary',
-    # T
-    # U
-    # V
-    # W
-    # X
-    # Y
-    'yes': 'Yes',
-    # Z
-}
-
-
+# A
+ADD = 'Add'
+# B
+# C
+CALCULATE = 'Calculate'
+COST = 'Cost'
+COUNT = 'COUNT'
+# D
+DELETE = 'Delete'
+DETAIL = 'Detail'
+# E
+EDIT = 'Edit'
+# F
+FULL_NAME = 'Full name'
+# G
+# H
+HOURLY_WORK = 'Hourly work'
+# I
+INFO = 'Info'
+ID = 'ID'
+# J
+# L
+LABORER = 'Laborer'
+LABORERS = 'Laborers'
+# K
+# M
+# N
+NO = 'No'
+NAME = 'Name'
+# O
+OK = 'OK'
+OPERATION = 'Operation'
+# P
+PRODUCT = 'Product'
+PIECEWORK = 'Piecework'
+PAYMENT = 'Payment'
+PATTERN_NAME = 'James Johnson'
+# Q
+# R
+RATE = 'Rate'
+# S
+SAVE = 'Save'
+SALARY = 'Salary'
+# T
+# U
+# V
+# W
+# X
+# Y
+YES = 'Yes'
+# Z
